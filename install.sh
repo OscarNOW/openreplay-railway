7e834d2fbb11324b0cf4f2d7f6ea59690804e88c
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-06-21 11:49:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install sudo -y
 wget https://raw.githubusercontent.com/openreplay/openreplay/main/scripts/helmcharts/openreplay-cli -O /bin/openreplay 
 chmod +x /bin/openreplay
-openreplay -i openreplay-railway-production.up.railway.app
+# openreplay -i openreplay-railway-production.up.railway.app
+openreplay -h
