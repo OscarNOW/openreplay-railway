c4ed82a1a0019637ddf0c1b6972b925cbdbc43d4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-06-21 12:09:57 +0200

    Added debug logs

--- install-2.sh
+++ install-2.sh
@@ -74,9 +74,13 @@ echo "Make sure that you have a Host A DNS record pointing to this instance!"
 echo "This will be used for TLS 🔐"
 echo -e "ie: my-openreplay.company.com (NOT an IP address)\n"
 
+echo "D1"
+
 echo -e "${GREEN}"
 
 echo "Got DOMAIN_NAME $DOMAIN_NAME from env"
+
+echo "D2"
 # read -rp "Enter DOMAIN_NAME: " DOMAIN_NAME
 
 echo -e "${NC}"
@@ -85,6 +89,8 @@ if [[ -z $DOMAIN_NAME ]]; then
 fi
 info "Using domain name: $DOMAIN_NAME 🌐"
 echo "CADDY_DOMAIN=\"$DOMAIN_NAME\"" >>common.env
+
+echo "D3"
 
 # read -p "Is the domain on a public DNS? (y/n) " yn
 # case $yn in
@@ -103,45 +109,58 @@ echo "CADDY_DOMAIN=\"$DOMAIN_NAME\"" >>common.env
 #     exit 1
 #     ;;
 # esac
+echo "D4"
 
 # Create passwords if they don't exist
 create_passwords
+echo "D5"
 
 info "Starting the application with Docker... 🐳"
 # Load variables from common.env into the current shell's environment
+echo "D6"
 set -a # automatically export all variables
+echo "D7"
 source common.env
+echo "D8"
 set +a
+echo "D9"
 
 # Use the `envsubst` command to substitute the shell environment variables into reference_var.env and output to a combined .env
 find ./ -type f \( -iname "*.env" -o -iname "docker-compose.yaml" \) ! -name "common.env" -exec /bin/bash -c 'file="{}"; git checkout -- "$file"; cp "$file" "$file.bak"; envsubst < "$file.bak" > "$file"; rm "$file.bak"' \;
 
-case $yn in
-y)
+echo "D10"
+# case $yn in
+# y)
     echo "$DOMAIN_NAME is on a public DNS"
     ##No changes needed
-    ;;
-n)
-    echo "$DOMAIN_NAME is on a private DNS"
+    # ;;
+# n)
+    # echo "$DOMAIN_NAME is on a private DNS"
     ##Add a variable to chalice.env file
-    echo "SKIP_H_SSL=True" >>chalice.env
-    ;;
-*)
-    echo invalid response
-    exit 1
-    ;;
-esac
+    # echo "SKIP_H_SSL=True" >>chalice.env
+    # ;;
+# *)
+    # echo invalid response
+    # exit 1
+    # ;;
+# esac
 
+echo "D11"
 readarray -t services < <(sudo -E docker-compose config --services)
+echo "D12"
 for service in "${services[@]}"; do
     echo "Pulling image for $service..."
     docker-compose pull --no-parallel "$service"
     sleep 5
 done
 
+echo "D13"
 docker-compose --profile migration up --force-recreate --build -d
+echo "D14"
 cp common.env common.env.bak
+echo "D15"
 echo "🎉🎉🎉  Done! 🎉🎉🎉"
+echo "D16"
 
 info "Installation complete!! open https://${DOMAIN_NAME} 🐳"
 info "${PWD} have the docker-compose file. you can use docker-compose stop/start"
